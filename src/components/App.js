6c010934b6288f30acc59a5e1b3bbcea0a5fdd44
lucifer1799 <47712039+lucifer1799@users.noreply.github.com>
2021-02-15 12:19:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,15 @@ import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 class App extends Component {
+
     render() {
+      const time=new Date().toLocaleTimeString()
 
         return(
-            <>
+            <div className='Clock'>
+              <h3 id='time'>{time}</h3>
                
-            </>
+            </div>
         )
     }
 }
